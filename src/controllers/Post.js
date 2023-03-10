a7c176fb0e8f870c039a76fef05bfaf6c5823dc2
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-03-09 22:23:08 -0300

    feat: get all posts, like and dislike posts

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,4 +1,4 @@
-import { insertPost } from '../repositories/postRepositories.js';
+import { deleteLikePost, getPosts, insertLikePost, insertPost } from '../repositories/PostRepositories.js';
 
 
 export async function publishPost(req, res) {
@@ -14,4 +14,46 @@ export async function publishPost(req, res) {
         console.log(error);
         return res.status(500).send(error);
     }
+}
+
+export async function likePost(req, res) {
+    const user_id = res.locals.id_user
+    const post_id = req.params.id
+
+    try {
+        await insertLikePost(user_id, post_id)
+        res.sendStatus(201)
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send(error);
+    }
+}
+
+export async function dislikePost(req, res) {
+    const user_id = res.locals.id_user
+    const post_id = req.params.id
+
+    try {
+        await deleteLikePost(user_id, post_id)
+        res.sendStatus(204)
+        
+    } catch(error){
+        console.log(error);
+        return res.status(500).send(error);
+    }
+}
+
+export async function getAllPosts(req, res){
+    
+    try {
+        const posts = await getPosts();
+        return res.status(200).send(posts)
+
+        
+    } catch(error){
+        console.log(error);
+        return res.status(500).send(error);
+    }
+
 }
--- src/repositories/postRepositories.js
+++
@@ -1,7 +0,0 @@
-import { db } from "../config/database.js"
-
-export async function insertPost(id_user, url, description){
-
-    return await db.query(`INSERT INTO post(id_user, url, description) VALUES ($1, $2, $3)`, [id_user, url, description])
-    
-}    
