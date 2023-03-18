6af2bdf7fd55dbb345481a7858260eddb652ce0b
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-03-18 12:53:23 -0300

    alter get all posts query

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -18,11 +18,11 @@ export async function publishPost(req, res) {
 
 
 export async function likePost(req, res) {
-    const user_id = res.locals.id_user
+    const id_user = res.locals.id_user
     const post_id = req.params.id
 
     try {
-        await insertLikePost(user_id, post_id)
+        await insertLikePost(id_user, post_id)
         res.sendStatus(201)
 
     } catch(error){
@@ -32,11 +32,11 @@ export async function likePost(req, res) {
 }
 
 export async function dislikePost(req, res) {
-    const user_id = res.locals.id_user
+    const id_user = res.locals.id_user
     const post_id = req.params.id
 
     try {
-        await deleteLikePost(user_id, post_id)
+        await deleteLikePost(id_user, post_id)
         res.sendStatus(204)
         
     } catch(error){
@@ -49,7 +49,7 @@ export async function getAllPosts(req, res){
     
     try {
         const posts = await getPosts();
-        return res.status(200).send(posts)
+        return res.status(200).send(posts.rows)
 
         
     } catch(error){
