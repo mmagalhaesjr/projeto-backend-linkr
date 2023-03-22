741caeac3c5a79e59582510a29d53d22e47beeb0
RubsRafa <rubiarafaela2000@gmail.com>
2023-03-22 15:55:02 -0300

    fix: rename reposts table to posts_shares

--- src/repositories/RePostRepositories.js
+++ src/repositories/RePostRepositories.js
@@ -5,8 +5,8 @@ export async function postExist(id){
 }
 
 export async function isReposted(id) {
-    return await db.query(`SELECT * FROM reposts WHERE id_post = $1;`, [id]);
+    return await db.query(`SELECT * FROM posts_shares WHERE id_post = $1;`, [id]);
 }
 export async function Repost(id, id_user){
-    return await db.query('INSERT INTO reposts (id_post, id_user) VALUES ($1, $2);', [id, id_user])
+    return await db.query('INSERT INTO posts_shares (id_post, id_user) VALUES ($1, $2);', [id, id_user])
 }
