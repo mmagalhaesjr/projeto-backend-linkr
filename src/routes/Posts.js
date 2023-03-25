f334a6aa33fd10f5430787149f218914a09030bf
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-03-25 14:33:12 -0300

    Nivelamento de código parcial

--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -5,6 +5,14 @@ import { editPost, getAllUsersPosts, getUserPosts, removePost } from "../control
 import { isUserAuthenticated } from "../middlewares/Authentication.js";
 import { verifyIfUserIsOwnerOfPost } from "../middlewares/Posts.js";
 
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { postSchema } from '../schemas/postSchema.js';
+import { validateToken } from '../middlewares/validateToken.js';
+import { dislikePost, likePost, publishPost } from '../controllers/Post.js';
+import { verifyIfPostExists } from '../middlewares/Post.js';
+import { commentSchema } from '../schemas/postSchema.js';
+import { insertNewComment } from '../controllers/Post.js';
+
 const router = express.Router();
 
 router.all("/posts", isUserAuthenticated, async (req, res) => {
@@ -26,5 +34,10 @@ router.all("/post/:id/update", isUserAuthenticated, verifyIfUserIsOwnerOfPost, a
     if (req.method === "PATCH") return await editPost(req, res);
     return res.status(405).send("this method is not allowed here");
 });
+
+router.post('/post', validateSchema(postSchema), validateToken, publishPost);
+router.post('/post/:id/like',validateToken, verifyIfPostExists, likePost);
+router.delete('/post/:id/dislike', validateToken, verifyIfPostExists, dislikePost);
+router.post('/comment/:id', validateSchema(commentSchema), validateToken, verifyIfPostExists, insertNewComment)
 
 export default router;
